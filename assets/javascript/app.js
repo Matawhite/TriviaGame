f2bb9a322d36311569cb0d603f1f013abda27aef
Matt White <mattwhite614@gmail.com>
2016-05-26 18:14:03 -0500

    controlers and views done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+var countDown = 5;
+
 //Models
 var score = {
   wins:0,
   loses:0,
-  "Not Answered": 0
+  notAnswered: 0
 };
 
 var questions = [
@@ -32,10 +34,8 @@ var questions = [
   //Start view
   function startView(){
     var indexNum = 0;
-    var  greeting =
-    '<h3>Press Start to Begin</h3>';
-    var button =
-    '<button id="startButtion" class="waves-effect waves-light btn">Start</button>';
+    var  greeting = '<h3>Press Start to Begin</h3>';
+    var button = '<button id="startButtion" class="waves-effect waves-light btn">Start</button>';
     $('#start').html(greeting);
     $('#start').append(button).on('click', function(){
       $('#start').empty();
@@ -51,12 +51,9 @@ var questions = [
       answerView(indexNum);
     };
 
-
-
-
-  //end Question and Answer view
-  function answerView(indexNum){
-  questions[indexNum].answers.forEach(function(answer){
+function answerView(indexNum){
+    startTimer(indexNum);
+    questions[indexNum].answers.forEach(function(answer){
       $('#answers').append('<li class="question">' + answer + '</li>')
         })
       $('.question').on('click', function(){
@@ -64,19 +61,27 @@ var questions = [
         checkAns(answer, indexNum);
       })
     }
-
+  //end Question and Answer view
   //Wrong Answer view
-  function wrongAns(indexNum) {
-    console.log('wrong');
-    console.log(indexNum);
+  function wrongAnsView(indexNum) {
+    stopTimer();
+    $('#answers').empty();
+    $('#timer').empty();
+    var msg =
+    '<p>Sorry your answer is incorrect<p>' +
+    '<p>The correct answer is ' + questions[indexNum].correctAnswer;
+    indexNum ++;
+    questionView(indexNum);
   }
   //end Wrong Answer view
 
   //Correct Answer view
-  function correctAns(answer,indexNum){
+  function correctAnsView(answer,indexNum){
+    stopTimer();
+    $('#answers').empty();
+    $('#timer').empty();
     var msg =
     '<p>'+ answer +' was correct!</p>';
-    $('#answers').empty();
     $('#questions').html(msg);
     indexNum ++;
     returnToQuestionView(indexNum);
@@ -84,30 +89,47 @@ var questions = [
   //end Correct Answer view
 
   //Score Board view
-
+  function scoreboardView(){
+    stopTimer();
+    var msg =
+    '<p>Wins: ' + score.wins + '</p> <p>Loses: ' + score.loses + '</p>';
+    $('#questions').html(msg);
+  };
   //end Score Board view
 
   // Update Timer view
-
+  function timerView(indexNum){
+    countDown --
+    $('#timer').html('<h2>' + countDown + '<h2>');
+    if(countDown === 0){
+      timesUpView(indexNum);
+    }
+  };
   //end Update Timer view
-
-
-
+  function timesUpView(indexNum){
+    questions.notAnswered ++;
+    $('#timer').empty();
+    $('#answers').empty();
+    $('questions').html('<h3>Time is up!</h3>')
+    .append('<p>The correct answer was ' + questions[indexNum].correctAnswer)
+    indexNum ++;
+    returnToQuestionView(indexNum)
+  }
 
 //Controllers
 function checkAns(answer, indexNum){
-  if(answer == questions[indexNum].correctAnswer){
+  if(answer === questions[indexNum].correctAnswer){
     score.wins ++;
-    correctAns(answer,indexNum);
+    correctAnsView(answer,indexNum);
   }else {
     score.loses ++;
-    wrongAns(indexNum);
+    wrongAnsView(indexNum);
   }
 };
 
-var returnToQuestionView = function (indexNum){
+ function returnToQuestionView(indexNum){
   if(indexNum >= questions.length){
-    console.log('scoreboard');
+    scoreboardView();
   }else{
   var newInterval = setTimeout(function() {
       questionView(indexNum);
@@ -115,6 +137,16 @@ var returnToQuestionView = function (indexNum){
  }
 }
 
+function startTimer(indexNum){
+  start = setInterval(function(){
+    timerView(indexNum)
+  }, 1000);
+}
+
+function stopTimer(){
+  countDown = 21;
+  clearInterval(start);
+}
 
 startView();
 }); //do not write below here. End of Document rdy fn
